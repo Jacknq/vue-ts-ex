e5013247ab31300fa390154be4748273cad4d27b
Jack85 <bousekr@gmail.com>
2016-11-30 16:33:53 +0100

    refact

--- src/System/router.ts
+++ src/System/router.ts
@@ -1,6 +1,6 @@
-import {Component, create, getHelper,Vue,Vuex }  from '../ext'
+import { Component, create, getHelper, Vue, Vuex } from '../ext'
 //import Router = require('vue-router')
-import {  RouterOptions,Location, RouteConfig, Route } from 'vue-router'
+import { RouterOptions, Location, RouteConfig, Route } from 'vue-router'
 import * as VueRouter from 'vue-router'
 
 
@@ -27,18 +27,17 @@ var about = require('../Views/about.vue').default
 //var app = Vue.extend({});
 //var ro :RouteConfig[] = [ { path:'/', component:VueRouter  }]
 
-const router = new VueRouter({ 
-   mode:'history',
-   routes:[
-     
-     { path: '/' },
-      {  path:'/ts', component:separatets},
-   {  path:'/about', component:about},
-  // { path: '/item/:id(\\d+)', component: Donor }, // component: ItemView },
-    { path: '/module/:compname' }, // component: UserView },
-      { path: '*', redirect: '/about'  },
-   // { path: '*', redirect: '/about'  }
-  //     path:'/', component:VueRouter
-  ]  
- });
- export default router
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+
+    { path: '/' },
+    { path: '/ts/:id(\\d+)', component: separatets },
+    { path: '/about', component: about },
+
+    { path: '/module/:compname' },
+    { path: '*', redirect: '/about' },
+
+  ]
+});
+export default router
