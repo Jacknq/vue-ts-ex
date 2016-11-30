abc079d905e9853411f9c727e78fc256203830e5
Jack85 <bousekr@gmail.com>
2016-11-30 16:37:22 +0100

    refact

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,22 +1,25 @@
 <template>
     <div>
 
-   <nav class="navbar navbar-default" v-bind:class="{'navbar-inverse' : inverted}">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-      
-       <h1> {{inverted}} /</h1>  <h1> {{someother}} nJOJOame</h1> 
-      
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li v-for="link in links" >
-                     <router-link :to="link.path">{{link.name}}</router-link>
-                    <!-- ROUTERLINK IS THE TRICK <a :href="link.path"></a></li>-->
-                </li>
-            </ul>
-        </div><!-- /.navbar-collapse  v-bind:class="{'active' : $route.path == link.path}"-->
-    </div><!-- /.container-fluid -->
-</nav>
+        <nav class="navbar navbar-default" v-bind:class="{'navbar-inverse' : inverted}">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+
+                <h1> {{inverted}} /</h1>
+                <h1> {{someother}} nJOJOame</h1>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li v-for="link in links">
+                            <router-link :to="link.path">{{link.name}}</router-link>
+                            <!-- ROUTERLINK IS THE TRICK <a :href="link.path"></a></li>-->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse  v-bind:class="{'active' : $route.path == link.path}"-->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
 </template>
 <script>
 import {Component, create, getHelper,Vue,Vuex, Prop, Watch }  from '../ext'
@@ -46,18 +49,17 @@ export default class navbar extends Vue {
           new Link('About', '/about'),
             new Link('type separate', '/ts')
       ]
-   // @Watch('$route.path')
    
      @Watch<navbar, string>( function(this, newVal?:any, oldVal?:any ){ // this.$route.path:string , newVal?:any, oldVal?:any 
          if(this.$route.path!=undefined)
          console.log('Changed current path to: ' + this.$route.path);
      })
      '$route.path'
-      // accessor is `computed`
-  //  get route() {
+      // test is `computed`
+  //  get test() {
     //    return 'asd';
    // }
-    //route = this.$route.path;
+    
     ready(){
         console.log(this.object.default+ this.$route.path);
     }
