d546a3a78cebe47e7bc9ae479479547653f071da
Jack85 <bousekr@gmail.com>
2016-12-08 13:30:51 +0100

    refact

---
+++ src/Views/home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        This Home template..{{someabout}}
+        </br>
+         <h3 @click="hello">
+            {{name}} is the best girl for {{i}} times
+            <button class="btn btn-secondary" @click.stop="add(2)">+</button>
+            <button class="btn btn-secondary" @click.stop="sub">-</button>
+          </h3>
+    </div>
+</template>
+<script lang="ts">
+import  {Component, create, getHelper,Vue,Vuex, Prop, Watch,Lifecycle }  from '../ext'
+ var multiselect = require('vue-multiselect').default;
+ import  store  from '../System/store'
+ var { getters, commit } = getHelper(store)
+
+
+ @Component({
+      name: 'home', components:{  multiselect:multiselect }
+ })
+export default class extends Vue {
+  name = 'home'
+  someabout = 'home'    
+  @Vuex i = getters('count')
+  @Vuex add = commit('increment')
+  @Vuex sub = commit('decrement')
+
+ @Lifecycle mounted() {
+       //here you show the alert
+       console.log('home mounted')      
+    }
+  hello() {
+    alert(this.name + ' is the best girl!') 
+  }
+}
+</script>
