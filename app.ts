99cd68d660317522a3b123ae5c36154c7a2ceedc
jack85 <bousekr@gmail.com>
2016-12-08 12:04:42 +0100

    refact

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ import { Component, create, getHelper, Vue, Vuex } from './src/ext'
 //import Router = require('vue-router')
 import { RouterOptions, Location, RouteConfig, Route } from 'vue-router'
 import * as VueRouter from 'vue-router'
-//import Vue = require('vue') abc++k00
+//import Vue = require('vue') abc++k0001
 import router from './src/System/router'
 var App = require('./app.vue').default
 Vue.use(VueRouter);
