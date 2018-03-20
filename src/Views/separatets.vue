080f0257b3c405a9b9a13ef367b90e0bf13dda02
Jack85 <bousekr@gmail.com>
2018-03-20 11:09:51 +0100

    refact

--- src/Views/separatets.vue
+++ src/Views/separatets.vue
@@ -1,10 +1,7 @@
 <template>
     <div>
       This one has separated Typescript.. {{someabout}}
-         <multiselect v-bind:selected="selected" 
-          v-bind:options="['option1','option2']" :showLabels="false"
-          :searchable='true' :allowEmpty='false'  >
-          </multiselect><br/>
+      
     </div>
 </template>
 <script src="./separatets.ts" lang="ts"/></script>
